e6d34d926a55d9a4e63da475cb480bf33389d493
ottovonrizzmark <majidimane05@gmail.com>
2024-10-05 21:06:00 +0200

    Add files via upload

---
+++ Goofy Aah Game.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  5 18:27:56 2024
+
+@author: majid
+"""
+
+low = 0
+high = 100
+
+print("=== Guessing Game ===\n")
+print("Please think of a number between 0 and 100!\n") 
+
+while True:
+    guess = (high + low) // 2
+    print(f"\nIs your secret number {guess}?\n") 
+    
+    x = input("Enter 'h' to indicate the guess is too high.\n"
+              "Enter 'l' to indicate the guess is too low.\n"
+              "Enter 'c' to indicate I guessed correctly.\n")
+
+    if x == 'c':
+        print(f"\nGame over LOOOOSAAAAAARRRRRR! Your secret number was: {guess}.\n")
+        break  
+
+    elif x == 'l':
+        low = guess 
+    elif x == 'h':
+        high = guess
+    else:
+        print("\nSorry, I did not understand your input. Please enter 'h', 'l', or 'c'.\n") 
+        continue 
+
+if low > high:
+    print("\nInconsistent input detected. Please check your responses.\n")
+else:
+    print("\nThank you for playing this game :3 Come back soon!\n")
+    
+        
+    
